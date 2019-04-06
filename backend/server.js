7c37401d5bf90056ee0fe42d7b646c15c961db0d
Paulius <paulius.gasiukevicius@gmail.com>
2019-04-06 16:26:26 +0300

    HTTPS! yay now app is secure! (self-signed though...)

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
+const https = require('https');
+const fs = require('file-system');
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const privateKey = fs.readFileSync(path.join(__dirname, 'key.pem'));
+const publicKey = fs.readFileSync(path.join(__dirname , 'certificate.pem'));
+
+const server = https.createServer({key: privateKey, cert: publicKey}, app);
+
+server.listen(process.env.PORT || 8080);
+
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,  '..', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.get('/test', function (req, res) {
+  res.send('OK ♣');
+});
