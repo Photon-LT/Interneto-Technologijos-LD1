5b1317328b07370f38e19916cb0d6627b6edb6e7
Paulius <paulius.gasiukevicius@gmail.com>
2019-04-06 16:39:41 +0300

    Apparently Heroku does not work with self-signed HTTPS...

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const http = require('http');
 const fs = require('file-system');
 const express = require('express');
 const path = require('path');
@@ -7,9 +8,9 @@ const app = express();
 const privateKey = fs.readFileSync(path.join(__dirname, 'key.pem'));
 const publicKey = fs.readFileSync(path.join(__dirname , 'certificate.pem'));
 
-const server = https.createServer({key: privateKey, cert: publicKey}, app);
-
-server.listen(process.env.PORT || 8080);
+http.createServer(app).listen(process.env.PORT || 8080);
+//https.createServer({key: privateKey, cert: publicKey}, app).listen(process.env.PORT || 8081);
+//^does not work for heroku
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
