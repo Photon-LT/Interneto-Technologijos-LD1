319552120100be055ada24d246aa2e1c8163bfa2
Paulius <paulius.gasiukevicius@gmail.com>
2019-04-06 16:55:38 +0300

    Attemps to force HTTPS on heroku

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+/*
 const privateKey = fs.readFileSync(path.join(__dirname, 'key.pem'));
 const publicKey = fs.readFileSync(path.join(__dirname , 'certificate.pem'));
-
-http.createServer(app).listen(process.env.PORT || 8080);
 //https.createServer({key: privateKey, cert: publicKey}, app).listen(process.env.PORT || 8081);
 //^does not work for heroku
+*/
+
+http.createServer(app).listen(process.env.PORT || 8080);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
